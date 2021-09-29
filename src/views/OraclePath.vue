72dcd209a47045ed1eb9c55650c49c93f903f8e8
LLFourn <lloyd.fourn@gmail.com>
2021-09-29 15:42:55 +1000

    ucfirst oracle path description

--- src/views/OraclePath.vue
+++ src/views/OraclePath.vue
@@ -7,7 +7,7 @@
     </v-row>
     <v-row class="text-center">
       <v-col>
-        <p v-html="$describe.path_html_str($route.params.path)"></p>
+        <p class="oracle-path-description" v-html="$describe.path_html_str($route.params.path)"></p>
       </v-col>
     </v-row>
     <v-row v-show="in_progress" justify="center">
@@ -112,3 +112,9 @@ export default {
   components: { ChildPicker, EventCard, RouterBreadcrumbs },
 };
 </script>
+
+<style scoped>
+  .oracle-path-description:first-letter {
+      text-transform:capitalize;
+  }
+</style>
